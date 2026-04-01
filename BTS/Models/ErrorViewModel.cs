7fa9c537ffabc59cc77ccea5260262846bf9129e
devpresss xmont <xmontemorjerald@gmail.com>
2026-04-02 00:13:39 +0800

    Initial commit

---
+++ BTS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
