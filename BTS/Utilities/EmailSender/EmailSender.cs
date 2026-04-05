26a3a01899c4fecd45cd8fa75001a2dbfa56e353
devpresss xmont <xmontemorjerald@gmail.com>
2026-04-05 16:07:35 +0800

    Added Email sender configuration using sendgrid

--- BTS/Utilities/EmailSender/EmailSender.cs
+++ BTS/Utilities/EmailSender/EmailSender.cs
@@ -1,14 +1,44 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.ComponentModel.DataAnnotations;
 
 namespace BTS.Utilities.EmailSender
 {
     public class EmailSender:IEmailSender
     {
-
-        public async Task SendEmailAsync (string subject, string body, string htmlMessage)
+        private readonly string? _sendGridAPIkey;
+        private readonly string? _from;
+        public EmailSender(IConfiguration config)
         {
-            await Task.CompletedTask;
+            _sendGridAPIkey = System.Environment.GetEnvironmentVariable("SEND_GRID_API_KEY ");
+            _from = System.Environment.GetEnvironmentVariable("SUPER_ADMIN_EMAIL");
+        }
+
+        public async Task SendEmailAsync (string email, string subject, string htmlMessage)
+        {
+
+            try
+            {
+                var sendGridClient = new SendGridClient(_sendGridAPIkey);
+                var from = new EmailAddress("developercsharp6@gmail.com","Password recovery");
+                var to = new EmailAddress(email);
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+                var response = await sendGridClient.SendEmailAsync(msg);
+                if (!response.IsSuccessStatusCode)
+                {
+                    var errorBody = await response.Body.ReadAsStringAsync();
+                    Console.WriteLine($"SendGrid Error [{response.StatusCode}]: {errorBody}");
+                    throw new Exception($"SendGrid failed: {errorBody}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to send email to {email}: {ex.Message}");
+            }
+
         }
     }
 }
